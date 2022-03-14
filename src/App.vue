38d14a6c8063157e73318256fedaf6eaab4adc56
bea <siteribea@gmail.com>
2022-03-14 07:22:05 +0100

    Add fake data for testing created lifecycle

--- src/App.vue
+++ src/App.vue
@@ -12,6 +12,33 @@ export default {
   components: {
     Header
   },
+  data() {
+    return {
+      tasks: []
+    }
+  },
+  created() {
+    this.task = [
+      {
+        id: 1,
+        text: 'Doctors Appointment',
+        day: 'March 1st at 2:30pm',
+        reminder: true
+      },
+      {
+        id: 2,
+        text: 'Meeting at school',
+        day: 'March 3st at 1:30pm',
+        reminder: true
+      },
+      {
+        id: 3,
+        text: 'Food Shopping',
+        day: 'March 3st at 11:00am',
+        reminder: false
+      }
+    ]
+  }
 };
 </script>
 
